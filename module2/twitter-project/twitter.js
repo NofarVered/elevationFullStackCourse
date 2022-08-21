e7afc78c6d4f39cd067f33e82845ea6a444607ba
NofarVered <nofarss79@gmail.com>
2022-08-21 18:26:38 +0300

    twitter tester

--- module2/twitter-project/twitter.js
+++ module2/twitter-project/twitter.js
@@ -1,16 +1,21 @@
-// Date.now() --- allows us to get the number of miliseconds elapsed since January 1, 1970
-
 const LogicTweeter = function () {
   let _DATA = [];
 
   const addPost = function (str) {
-    _DATA.push({ text: str, id: "p" + Date.now(), comments: [] });
+    _DATA.push({
+      text: str,
+      id: "p" + Math.floor(Math.random() * 100),
+      comments: [],
+    });
   };
 
   const addComment = function (curId, str) {
     _DATA.forEach((post) => {
       if (curId == post.id) {
-        post.comments.push({ id: "c" + Date.now(), test: str });
+        post.comments.push({
+          id: "c" + Math.floor(Math.random() * 100),
+          test: str,
+        });
       }
     });
   };
@@ -39,28 +44,3 @@ const LogicTweeter = function () {
     getPosts: getPosts,
   };
 };
-
-const tweeter = LogicTweeter();
-function tester() {
-  tweeter.addPost("This is my own post!");
-  tweeter.addPost("2");
-  tweeter.addPost("3");
-  tweeter.addPost("4");
-  tweeter.addPost("5");
-  console.log(tweeter.getPosts());
-
-  tweeter.removePost("p2");
-  tweeter.removePost("p3");
-  console.log(tweeter.getPosts());
-
-  tweeter.addComment("p1", "aiiiii");
-  tweeter.addComment("p1", "biiiii");
-  tweeter.addComment("p2", "ciiiii");
-  tweeter.addComment("p4", "diiiii");
-  tweeter.addComment("p5", "eiiiii");
-  console.log(tweeter.getPosts());
-
-  tweeter.removeComment("p1", "c2");
-  tweeter.removeComment("p4", "c3");
-  console.log(tweeter.getPosts());
-}
