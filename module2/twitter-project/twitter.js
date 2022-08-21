e27aab7162ae4504a3726a950bcb4ff3b612a30a
NofarVered <nofarss79@gmail.com>
2022-08-21 18:08:33 +0300

    twitter

--- module2/twitter-project/twitter.js
+++ module2/twitter-project/twitter.js
@@ -1,1 +1,41 @@
-const Twitter = {};
+// Date.now() --- allows us to get the number of miliseconds elapsed since January 1, 1970
+
+const Twitter = function () {
+  let _DATA = [];
+
+  const addPost = function (str) {
+    _DATA.push({ text: str, id: "p" + Date.now(), comments: [] });
+  };
+
+  const addComment = function (curId, str) {
+    _DATA.forEach((post) => {
+      if (curId == post.id) {
+        post.comments.push({ id: "c" + Date.now(), test: str });
+      }
+    });
+  };
+
+  const removePost = function (curId) {
+    _DATA = _DATA.filter((post) => post.id !== curId);
+  };
+
+  const removeComment = function (postId, comId) {
+    _DATA.forEach((post) => {
+      if (post.id == postId) {
+        post.comments = post.comments.filter((c) => c.id !== comId);
+      }
+    });
+  };
+
+  const getPosts = function () {
+    console.log(_DATA);
+  };
+
+  return {
+    addPost: addPost,
+    addComment: addComment,
+    removePost: removePost,
+    removeComment: removeComment,
+    getPosts: getPosts,
+  };
+};
