0610303ee887cd715bfc113fa8311ad8dd80d1b5
NofarVered <nofarss79@gmail.com>
2022-08-21 18:15:57 +0300

    twitter tester

--- module2/twitter-project/twitter.js
+++ module2/twitter-project/twitter.js
@@ -1,6 +1,6 @@
 // Date.now() --- allows us to get the number of miliseconds elapsed since January 1, 1970
 
-const Twitter = function () {
+const LogicTweeter = function () {
   let _DATA = [];
 
   const addPost = function (str) {
@@ -39,3 +39,28 @@ const Twitter = function () {
     getPosts: getPosts,
   };
 };
+
+const tweeter = LogicTweeter();
+function tester() {
+  tweeter.addPost("This is my own post!");
+  tweeter.addPost("2");
+  tweeter.addPost("3");
+  tweeter.addPost("4");
+  tweeter.addPost("5");
+  console.log(tweeter.getPosts());
+
+  tweeter.removePost("p2");
+  tweeter.removePost("p3");
+  console.log(tweeter.getPosts());
+
+  tweeter.addComment("p1", "aiiiii");
+  tweeter.addComment("p1", "biiiii");
+  tweeter.addComment("p2", "ciiiii");
+  tweeter.addComment("p4", "diiiii");
+  tweeter.addComment("p5", "eiiiii");
+  console.log(tweeter.getPosts());
+
+  tweeter.removeComment("p1", "c2");
+  tweeter.removeComment("p4", "c3");
+  console.log(tweeter.getPosts());
+}
