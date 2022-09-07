27e03a6a8c0f29c258d569f02294c03717481a58
NofarVered <nofarss79@gmail.com>
2022-09-07 10:30:01 +0300

    profile project

---
+++ profileProject/build/render.js
@@ -0,0 +1,16 @@
+'use strict';
+//class which should render each section of the user page through Handlebars
+// class render{
+// Random User Generator API
+// picture, a first name & last name, the city and state.
+// userApiGenerator():{
+// }
+// Random Kanye Quote Generator API
+// PokeAPI
+// Bacon Ipsum API
+// }
+$.get('https://randomuser.me/api/?results=7')
+  .then((res) => {
+    console.log(res.results[0].name);
+  })
+  .catch((error) => console.error(error));
---
+++ profileProject/src/render.ts
@@ -0,0 +1,12 @@
+//class which should render each section of the user page through Handlebars
+// class render{
+    // Random User Generator API
+    // picture, a first name & last name, the city and state.
+    // userApiGenerator():{
+
+    // }
+    // Random Kanye Quote Generator API
+    // PokeAPI
+    // Bacon Ipsum API
+// }
+$.get("https://randomuser.me/api/?results=7").then((res)=>{console.log(res)}).catch((error)=> console.error(error))
