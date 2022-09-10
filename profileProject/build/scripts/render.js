72c911b27a4ddc72f57287ef43d80e56c42d05b2
NofarVered <nofarss79@gmail.com>
2022-09-10 18:29:49 +0300

    template bar handeling

--- profileProject/build/scripts/render.js
+++ profileProject/build/scripts/render.js
@@ -1,11 +1,33 @@
 "use strict";
-//class which should render each section of the user page through Handlebars
-// class render{
-// Random User Generator API
-// picture, a first name & last name, the city and state.
-// userApiGenerator():{
-// }
-// Random Kanye Quote Generator API
-// PokeAPI
-// Bacon Ipsum API
-// }
+class render {
+    static clean() {
+        $("#friends-div").empty();
+        $("#user-div").empty();
+        $("#pokemon-div").empty();
+        $("#quote-div").empty();
+        $("#about-div").empty();
+    }
+    static renderPerson(user) {
+        let source = $("#user-template").html();
+        let template = Handlebars.compile(source);
+        $("#user-div").append(template(user));
+        source = $("#friends-template").html();
+        template = Handlebars.compile(source);
+        $("#friends-div").append(template({ friends: user.friends }));
+    }
+    static renderQuote(quote) {
+        const source = $("#quote-template").html();
+        let template = Handlebars.compile(source);
+        $("#quote-div").append(template(quote));
+    }
+    static renderPokemon(pokemon) {
+        const source = $("#pokemon-template").html();
+        let template = Handlebars.compile(source);
+        $("#pokemon-div").append(template(pokemon));
+    }
+    static renderAboutMe(about) {
+        const source = $("#about-template").html();
+        let template = Handlebars.compile(source);
+        $("#about-div").append(template(about));
+    }
+}
--- profileProject/src/scripts/render.ts
+++ profileProject/src/scripts/render.ts
@@ -1,11 +1,34 @@
-//class which should render each section of the user page through Handlebars
-// class render{
-    // Random User Generator API
-    // picture, a first name & last name, the city and state.
-    // userApiGenerator():{
+class render{
+    static clean():void {
+        $("#friends-div").empty();
+        $("#user-div").empty();
+        $("#pokemon-div").empty();
+        $("#quote-div").empty();
+        $("#about-div").empty();
+    }
+    static renderPerson(user:Person):void {
+        let source = $("#user-template").html();
+        let template = Handlebars.compile(source);
+        $("#user-div").append(template(user));
+        
+        source = $("#friends-template").html();
+        template = Handlebars.compile(source);
+        $("#friends-div").append(template({ friends: user.friends }));
+    }
+    static renderQuote(quote: Quote):void {
+        const source = $("#quote-template").html();
+        let template = Handlebars.compile(source);
+        $("#quote-div").append(template(quote));
+    }
+    static renderPokemon(pokemon: Pokemon):void {
+        const source = $("#pokemon-template").html();
+        let template = Handlebars.compile(source);
+        $("#pokemon-div").append(template(pokemon));
+    }
+    static renderAboutMe(about: About):void {
+        const source = $("#about-template").html();
+        let template = Handlebars.compile(source);
+        $("#about-div").append(template(about));
+    }
 
-    // }
-    // Random Kanye Quote Generator API
-    // PokeAPI
-    // Bacon Ipsum API
-// }
+}
